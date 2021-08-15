9115a229f1c77dfce63394f5d40a99cafe25a10e
Aastha Neupane <aastha.pane12@gmail.com>
2021-08-15 13:31:46 -0400

    spacing fixed

--- src/main/java/com/assignment/productscategories/services/ProductService.java
+++ src/main/java/com/assignment/productscategories/services/ProductService.java
@@ -1,5 +1,4 @@
 package com.assignment.productscategories.services;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,19 +11,16 @@ import com.assignment.productscategories.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-
 	private final ProductRepository productRepo;
 	private final CategoryProductRepository catProdRepo;
-	
+
 	public ProductService(ProductRepository productRepo, CategoryProductRepository catProdRepo) {
 		this.productRepo = productRepo;
 		this.catProdRepo = catProdRepo;
 	}
-	
 	public List<Product> allProducts(){
 		return productRepo.findAll();
 	}
-	
 	public Product findProduct(Long id) {
 		Optional<Product> optProd = productRepo.findById(id);
 		if(optProd.isPresent()) {
@@ -33,18 +29,13 @@ public class ProductService {
 			return null;
 		}
 	}
-	
 	public Product saveProduct(Product product) {
 		return productRepo.save(product);
 	}
-	
 	public void deleteProduct(Long id) {
 		productRepo.deleteById(id);
 	}
-	
 	public CategoryProduct saveRelationship(CategoryProduct cp) {
 		return catProdRepo.save(cp);
 	}
-	
-	
 }
